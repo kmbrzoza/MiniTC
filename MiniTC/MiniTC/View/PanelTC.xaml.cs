6c8605b834f6a69c5d062250eb8ab0d50a1a50f1
Brzoza Krzysztof <kmbrzoza@gmail.com>
2021-05-16 13:35:55 +0200

    Added binding to PanelTC Control

--- MiniTC/MiniTC/View/PanelTC.xaml.cs
+++ MiniTC/MiniTC/View/PanelTC.xaml.cs
@@ -24,5 +24,45 @@ namespace MiniTC.View
         {
             InitializeComponent();
         }
+
+        public static readonly DependencyProperty PathStringProperty = DependencyProperty.Register(
+        "PathString", typeof(string), typeof(PanelTC), new FrameworkPropertyMetadata(null));
+        public string PathString
+        {
+            get { return (string)GetValue(PathStringProperty); }
+            set { SetValue(PathStringProperty, value); }
+        }
+
+        public static readonly DependencyProperty DrivesProperty = DependencyProperty.Register(
+        "Drives", typeof(string[]), typeof(PanelTC), new FrameworkPropertyMetadata(null));
+        public string[] Drives
+        {
+            get { return (string[])GetValue(DrivesProperty); }
+            set { SetValue(DrivesProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedDriveProperty = DependencyProperty.Register(
+        "SelectedDrive", typeof(int), typeof(PanelTC), new FrameworkPropertyMetadata(null));
+        public int SelectedDrive
+        {
+            get { return (int)GetValue(SelectedDriveProperty); }
+            set { SetValue(SelectedDriveProperty, value); }
+        }
+
+        public static readonly DependencyProperty AllFilesProperty = DependencyProperty.Register(
+        "AllFiles", typeof(string[]), typeof(PanelTC), new FrameworkPropertyMetadata(null));
+        public string[] AllFiles
+        {
+            get { return (string[])GetValue(AllFilesProperty); }
+            set { SetValue(AllFilesProperty, value); }
+        }
+
+        public static readonly DependencyProperty SelectedFileProperty = DependencyProperty.Register(
+        "SelectedFile", typeof(int), typeof(PanelTC), new FrameworkPropertyMetadata(null));
+        public int SelectedFile
+        {
+            get { return (int)GetValue(SelectedFileProperty); }
+            set { SetValue(SelectedFileProperty, value); }
+        }
     }
 }
