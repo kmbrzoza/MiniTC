bbdbbacbf431bea0d4bb1f31075f3ea52c773c29
Brzoza Krzysztof <kmbrzoza@gmail.com>
2021-05-16 09:48:42 +0200

    Fixed project structure files

--- MiniTC/MiniTC/ViewModel/BaseClass/ViewModel.cs
+++ MiniTC/MiniTC/ViewModel/BaseClass/ViewModel.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TicTacToe.ViewModel.BaseClass
+namespace MiniTC.ViewModel.BaseClass
 {
     class ViewModel : INotifyPropertyChanged
     {
