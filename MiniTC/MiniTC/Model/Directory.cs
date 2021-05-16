875abbde76d39dc6659306a2352610f03bf69a6b
Brzoza Krzysztof <kmbrzoza@gmail.com>
2021-05-16 13:04:37 +0200

    Added directory class

---
+++ MiniTC/MiniTC/Model/Directory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniTC.Model
+{
+    class Directory
+    {
+        public string DirectoryPath { get; private set; }
+        public string Name { get { return System.IO.Path.GetDirectoryName(DirectoryPath); } }
+        private System.IO.DirectoryInfo fileInfo;
+
+        public Directory(string directoryPath)
+        {
+            this.DirectoryPath = directoryPath;
+            fileInfo = new System.IO.DirectoryInfo(directoryPath);
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
