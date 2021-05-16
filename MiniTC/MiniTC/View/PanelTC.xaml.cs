08743a71db9ac907451c37550697bf55df103acb
Brzoza Krzysztof <kmbrzoza@gmail.com>
2021-05-16 13:22:05 +0200

    Premilinary view for PanelTC Control

---
+++ MiniTC/MiniTC/View/PanelTC.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MiniTC.View
+{
+    /// <summary>
+    /// Interaction logic for PanelTC.xaml
+    /// </summary>
+    public partial class PanelTC : UserControl
+    {
+        public PanelTC()
+        {
+            InitializeComponent();
+        }
+    }
+}
