ba232d726c7b5831f99c5baee73484a30aebf5eb
Brzoza Krzysztof <kmbrzoza@gmail.com>
2021-05-18 17:52:36 +0200

    Added copying folders

--- MiniTC/MiniTC/ViewModel/MainViewModel.cs
+++ MiniTC/MiniTC/ViewModel/MainViewModel.cs
@@ -36,21 +36,22 @@ namespace MiniTC.ViewModel
                         arg =>
                         {
                             var selFile = Left.GetSelectedFile();
-                            
+                            if (selFile == null) return;
+
+                            string sourceFileName = selFile.Name;
+                            string sourceFilePath = selFile.Path;
+                            string destinationDir = Right.ActualPath;
+                            string destinationPath = $@"{destinationDir}\{sourceFileName}";
+
+                            var freeSpace = Right.GetFreeSpaceFromSelectedDirve();
+                            // disk error
+                            if (freeSpace == null)
+                            {
+                                MessageBox.Show("Disk error (check if your disk is connected)!", "Error!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                return;
+                            }
                             if (selFile is FileObj srcFile)
                             {
-                                string sourceFileName = srcFile.Name;
-                                string sourceFilePath = srcFile.Path;
-                                string destinationDir = Right.ActualPath;
-                                string destinationPath = $@"{destinationDir}\{sourceFileName}";
-
-                                var freeSpace = Right.GetFreeSpaceFromSelectedDirve();
-                                // disk error
-                                if (freeSpace == null)
-                                {
-                                    MessageBox.Show("Disk error (check if your disk is connected)!", "Error!", MessageBoxButton.OK, MessageBoxImage.Warning);
-                                    return;
-                                }
                                 // checking if file exists
                                 if (File.Exists(destinationPath))
                                 {
@@ -63,9 +64,37 @@ namespace MiniTC.ViewModel
                                     MessageBox.Show("Out of disk space!", "Error!", MessageBoxButton.OK, MessageBoxImage.Warning);
                                     return;
                                 }
+                                // copying
                                 File.Copy(sourceFilePath, destinationPath);
-                                Right.UpdateFiles();
                             }
+
+                            if (selFile is DirectoryObj srcDir)
+                            {
+                                // checking if directory exists
+                                if (Directory.Exists(destinationPath))
+                                {
+                                    MessageBox.Show("Such a directory already exists!", "Error!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                    return;
+                                }
+                                // getting space (weight) from all files
+                                long totalSpace = 0;
+                                foreach (string filePath in Directory.GetFiles(sourceFilePath, "*.*", SearchOption.AllDirectories))
+                                    totalSpace += new FileInfo(filePath).Length;
+
+                                // if there is no space 
+                                if (freeSpace < totalSpace)
+                                {
+                                    MessageBox.Show("Out of disk space!", "Error!", MessageBoxButton.OK, MessageBoxImage.Warning);
+                                    return;
+                                }
+                                // copying - recursively (first all directories, next all files)
+                                foreach (string dirPath in Directory.GetDirectories(sourceFilePath, "*", SearchOption.AllDirectories))
+                                    Directory.CreateDirectory(dirPath.Replace(sourceFilePath, destinationPath));
+                                foreach (string filePath in Directory.GetFiles(sourceFilePath, "*.*", SearchOption.AllDirectories))
+                                    File.Copy(filePath, filePath.Replace(sourceFilePath, destinationPath), true);
+                            }
+
+                            Right.UpdateFiles();
                         },
                         arg => true
                         );
